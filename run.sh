a711965a446313f26c7d61b6903c7674d8543254
Hugo Agbonon <hugo@agbonon.fr>
2015-12-29 17:01:52 +0100

    utilitary script to generate data

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# move files around until jsonresume allows other files that resume.json
+resume export --theme kendall --format html resume.html;
+mv resume.json resume.en.json;
+mv cv.json resume.json;
+resume export --theme kendall --format html cv.html;
+mv resume.json cv.json;
+mv resume.en.json resume.json;
+node augmentFiles.js;
